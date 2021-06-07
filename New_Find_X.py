b2bf29b8877a68650ecfe8aeae2c9d1ef684260c
Tony <54785394+Crazyblob@users.noreply.github.com>
2021-06-07 19:53:02 -0400

    Improved New_Find_X function
    Improved the New_Find_X function by allowing the input of the equation as an argument, allowing it to be used by other programs / files. Everything inside the function is now isolated, now not needing anything outside of it to function. Imposing the second function might be a challenge for this reason. test_stuff tests the function, with success!

--- New_Find_X.py
+++ New_Find_X.py
@@ -1,5 +1,3 @@
-#findx program
-
 #equation = newvar + 4 * 3 #divides the equation in both sides (answer + expression)
 # 4x + 3 = 15 is actually x = (15 - 3)/4
 
@@ -7,34 +5,27 @@
 #the equation by going through the equation with the position of operators in order of 
 #operations.
 
-import time
-
-OperatorSigns = "-+*/÷^"
-
-equation = input("Submit your equation: ")
-
-def ImprovedMarkOperator():
+def ImprovedMarkOperator(equation):
     EPosition = 0
     OSPosition = 0
     AddtoList = []
-    checkdone = False
+    OperatorSigns = "-+*/÷^"
     while EPosition != len(equation) - 1:
     #one letter equation won't run (it is len(equation) - 1)
         while True:
-            #originally was while checkdone == False:
             if equation[EPosition] == OperatorSigns[OSPosition]:
                 AddtoList.append(EPosition)
                 break
-                checkdone == True
             elif OSPosition == len(OperatorSigns) - 1:
                 break
-                checkdone = True
             else:
                 OSPosition = OSPosition + 1
         OSPosition = 0
         EPosition = EPosition + 1
-        #checkdone = False
     return AddtoList
 
-OperatorPositions = ImprovedMarkOperator()
-print(OperatorPositions)
+#OperatorPositions = ImprovedMarkOperator(input('Submit your equation: '))
+#print(OperatorPositions)
+
+#def calculate():
+#    OSPPosition = 0
---
+++ test_stuff.py
@@ -0,0 +1,5 @@
+import New_Find_X
+
+variable = New_Find_X.ImprovedMarkOperator('4 + 3 / 2 ^ 3 - 2')
+
+print(variable)
